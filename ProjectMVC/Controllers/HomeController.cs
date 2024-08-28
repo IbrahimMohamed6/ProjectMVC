83e312aa62a04e5d3cbbd33e9aaace87fbe62b75
Ibrahim Mohamed <hemaabokhalil677@gmail.com>
2024-08-28 06:01:22 +0300

    First Commit

---
+++ ProjectMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace ProjectMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUS()
+        {
+
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+
+            return View();
+        }
+
+        public ActionResult Privecy()
+        {
+            return View();
+        }
+
+    }
+}
--- ProjectMVC/Program.cs
+++ ProjectMVC/Program.cs
@@ -5,11 +5,53 @@ namespace ProjectMVC
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            builder.Services.AddControllersWithViews();
 
-            app.MapGet("/", () => "Hello World!");
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            #region MyRegion
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/Home", async context =>
+            //    {
+            //        await context.Response.WriteAsync("You Are At Home Page");
+            //    });
+            //});
+
+
+
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/Products", async context =>
+            //    {
+            //        await context.Response.WriteAsync("You Are At Products Page");
+            //    });
+            //});
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/Products/{id}", async context =>
+            //    {
+            //        int id =Convert.ToInt32( context.Request.RouteValues["id"]);
+            //        await context.Response.WriteAsync($"You Are At Products Page Wih id => {id}");
+            //    });
+            //}); 
+            #endregion
+
+
+            app.MapControllerRoute(
+                name: "defualt",
+                pattern: "/{controller=Home}/{Action=Index}",
+                defaults: new { controller = "Home", Action = "Index"}
+                );
 
             app.Run();
+
+
+            
         }
     }
 }
